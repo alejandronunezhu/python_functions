c0b8292dd1ac51490e109d5ac2cdb7ca45187bb9
Ale <79229219+alejandronunezhu@users.noreply.github.com>
2025-07-14 10:14:50 +0100

    Create verify_card_number.py

---
+++ verify_card_number/verify_card_number.py
@@ -0,0 +1,35 @@
+def verify_card_number(card_number):
+    """Return True if card number is valid according to the Luhn algorithm."""
+    sum_of_odd_digits = 0
+    card_number_reversed = card_number[::-1]
+    odd_digits = card_number_reversed[::2]
+
+    for digit in odd_digits:
+        sum_of_odd_digits += int(digit)
+
+    sum_of_even_digits = 0
+    even_digits = card_number_reversed[1::2]
+    for digit in even_digits:
+        number = int(digit) * 2
+        if number >= 10:
+            number = (number // 10) + (number % 10)
+        sum_of_even_digits += number
+    total = sum_of_odd_digits + sum_of_even_digits
+    return total % 10 == 0
+
+def main():
+    card_number = input("Enter credit card number (with or without spaces or dashes): ")
+    card_translation = str.maketrans({'-': '', ' ': ''})
+    translated_card_number = card_number.translate(card_translation)
+
+    if not translated_card_number.isdigit():
+        print("Invalid input: card number must contain only digits.")
+        return
+
+    if verify_card_number(translated_card_number):
+        print('VALID!')
+    else:
+        print('INVALID!')
+
+if __name__ == "__main__":
+    main()
