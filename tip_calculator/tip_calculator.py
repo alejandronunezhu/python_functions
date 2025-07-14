25068ff0ce0d283cd2f75c5edc283bfef14a349d
Ale <79229219+alejandronunezhu@users.noreply.github.com>
2025-07-14 14:36:43 +0100

    Update tip_calculator.py

--- tip_calculator/tip_calculator.py
+++ tip_calculator/tip_calculator.py
@@ -1,16 +1,16 @@
-def main():
+def main() -> None:
     dollars = dollars_to_float(input("How much was the meal? "))
     percent = percent_to_float(input("What percentage would you like to tip? "))
     tip = dollars * percent
     print(f"Leave ${tip:.2f}")
 
-def dollars_to_float(d):
+def dollars_to_float(d: str) -> float:
     float_amount = ""
     for char in d[1:-1]:
         float_amount += char
     return float(float_amount)
 
-def percent_to_float(p):
+def percent_to_float(p: str) -> float:
     float_percent = ""
     for char in p[0:-1]:
         float_percent += char
